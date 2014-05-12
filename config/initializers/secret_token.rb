42d8ebe068d24ab0c991ff85fb80c24b9a26dc46
Apprentice <apprentice@dbc-chicago-6.local>
2014-05-12 15:57:59 -0500

    Users setup w/ Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Barter::Application.config.secret_key_base = '7c03333dbc26cebf72c310e54c371b7cd981826044e48cd06c64d70dba68d2fb24cb5bee1792ed7366d4ebc06b4c5a65c91958c64a55cd990d828d5ced7307c8'
